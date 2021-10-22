8d6aaa820b2948579f6ad5d528dd698beb696964
Yanisdj95 <yanis.djebbari@ynov.com>
2021-10-22 15:38:02 +0200

    navbar fini

--- jvmarketplace/src/components/navbar/index.js
+++ jvmarketplace/src/components/navbar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components'
 import { Link,useHistory, Router } from 'react-router-dom';
 import home from '../../assets/home.png'
+import back from '../../assets/back.png'
 const Navbar = props => {
     const history = useHistory()
     return (
@@ -11,9 +12,13 @@ const Navbar = props => {
                     <StyledImg src={home} onClick={() => history.push('/')}></StyledImg>
                 </LeftHeader>
                 <RightHeader>
-                    <StyledItem onClick={() => history.push('/jeux')}>Jeux</StyledItem>
-                    <StyledItem onClick={() => history.push('/favorites')}>Favoris</StyledItem>
-                    <StyledItem onClick={() => history.goBack()}> Retour</StyledItem>
+                    <StyledItem onClick={() => history.push('/jeux')}>
+                        <StyledText>Jeux</StyledText>
+                    </StyledItem>
+                    <StyledItem onClick={() => history.push('/favorites')}>
+                        <StyledText>Favoris</StyledText>
+                    </StyledItem>
+                        <StyledImg src={back} onClick={() => history.goBack()}></StyledImg>
                 </RightHeader>
             </StyledDiv>
         </nav>
@@ -22,7 +27,6 @@ const Navbar = props => {
 
 const StyledDiv = styled.div`
     display: flex;
-    background-color:red;
     padding:12px;
 `
 const LeftHeader = styled.div`
@@ -33,19 +37,31 @@ const RightHeader = styled.div`
     justify-content: right;
     display:flex;
     flex:8;
-    background:green;
 `
 const StyledImg = styled.img`
     display: flex;
     justify-content: center;
     width:60px;
     height:60px;
+    margin-left:40px;
+    margin-right:40px;
 `
 
-const StyledItem = styled.p`
+const StyledItem = styled.div`
     display:flex;
     background-color: blue;
-    margin-left:40px;
+    margin-left:10px;
+    width:70px;
+    border: solid;
+    &:hover {
+        background-color:blueviolet
+    }
+`
+
+const StyledText = styled.p`
+    color:white;
+    margin-left: auto;
+    margin-right: auto;
 `
 
 
