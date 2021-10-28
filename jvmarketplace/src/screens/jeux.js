f73b8c2b139a52beae19eb2f8fd16d2c2d27cf6a
Yanisdj95 <yanis.djebbari@ynov.com>
2021-10-28 08:58:19 +0200

    page anime

--- jvmarketplace/src/screens/jeux.js
+++ jvmarketplace/src/screens/jeux.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 const Jeux = props => {
+    const [apiData, setApiData] = useState([])
+
+    useEffect(() => {
+        const apiData = axios({
+            method:'get',
+            url: "https://api.jikan.moe/v3/top/anime",
+        }).then((response) => {
+            setApiData(response.data.top)
+        }).catch(err => {
+            console.log(err)
+        })
+      }, [])
+      console.log(apiData)
     return (
-        <div>
-            <p>Page jeux</p>
-        </div>
+        <ContainerDiv>
+            {apiData.map(anime => 
+                <AnimeDiv>
+                        <TitreDive>
+                            <p>{anime.title}</p>
+                        </TitreDive>
+                        <img src={anime.image_url}/>
+                </AnimeDiv>
+            )}
+        </ContainerDiv>
     );
 };
+const ContainerDiv = styled.div`
+    display:flex;
+    flex-wrap: wrap;
+`
+const AnimeDiv = styled.div`
+    background-color: red;
+    height:600px;
+    width:300px;
+    margin:30px;
+`
+const TitreDive = styled.div`
+    height:100px;
+    width:100px;
+    background-color: aliceblue;
+`
+
 
 export default Jeux;
