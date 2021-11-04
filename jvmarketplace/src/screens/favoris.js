d518fd170cfe184b79ee21ca4111e5892509e5ad
Yanisdj95 <yanis.djebbari@ynov.com>
2021-11-04 14:22:46 +0100

    remove fav

--- jvmarketplace/src/screens/favoris.js
+++ jvmarketplace/src/screens/favoris.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components'
 const Favoris = props => {
-    const [fav, setFav] = useState(JSON.parse(localStorage.getItem('favorites')))
+    const favoriteCheck = localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : []
+    const [favorites, setFav] = useState(favoriteCheck)
     useEffect(() => {
-        console.log(fav)
+        console.log(favorites)
     }, [])
+    
+    const removeFavorite = (id) => {
+        console.log("🚀 ~ file: characterDetails.js ~ line 44 ~ removeFavorite ~ id", id)
+            const newFavorite = favorites.filter(favorites => favorites.id !== Number(id))
+            console.log("🚀 ~ file: characterDetails.js ~ line 45 ~ removeFavorite ~ newFavorite", newFavorite)
+            localStorage.setItem('favorites', JSON.stringify(newFavorite)) 
+            setFav(newFavorite) 
+        }
     return (
         <ContainerDiv>
-                {fav.map(anime =>
+                {favorites.map(anime =>
                     <AnimeDiv>
                     <TitreDive>
                         <StyledTitre>{anime.title}</StyledTitre>
                     </TitreDive>
                     <StyledImg src={anime.image}/>
-                    <button>REMOVE</button>
+                    <button onClick={() => removeFavorite(anime.id)}>REMOVE</button>
                 </AnimeDiv>
                 )}
         </ContainerDiv>
